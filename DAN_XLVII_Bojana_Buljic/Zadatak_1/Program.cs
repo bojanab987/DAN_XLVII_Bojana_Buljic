6a2822a6ade1d3e2acfe4b234d916aa58329df23
bojanabb <bojana.buljic@gmail.com>
2020-07-16 13:34:34 +0200

    Add Vehicle class and method for printing announcments

--- DAN_XLVII_Bojana_Buljic/Zadatak_1/Program.cs
+++ DAN_XLVII_Bojana_Buljic/Zadatak_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace Zadatak_1
 {
     class Program
     {
+        /// <summary>
+        /// Method for generating number of vehicles
+        /// </summary>
+        /// <returns></returns>
+        static int GetNoOfVehicle()
+        {
+            Random rnd = new Random();
+            int noOfVehicles = rnd.Next(1, 16);
+            return noOfVehicles;
+        }
+
+        /// <summary>
+        /// Method for printing notifications about total number of vehicles and oreder and direction of each vehicle
+        /// </summary>
+        /// <param name="vehicles"></param>
+        public static void PrintVehiclesCreated(Vehicle[] vehicles)
+        {
+            Console.WriteLine("Total number of vehicles is: {0}", vehicles.Length);
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                Console.WriteLine("Vehicle no " + vehicles[i].OrderNo + "goes to direction: " + vehicles[i].DirectionV);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Stopwatch swApp = new Stopwatch();
+            swApp.Start();
+            int num = GetNoOfVehicle();
+            Vehicle[] vehicles = new Vehicle[num];
+            for (int i = 0; i < num; i++)
+            {
+                Vehicle car = new Vehicle(i + 1);
+                vehicles[i] = car;
+            }
+            PrintVehiclesCreated(vehicles);
+
+            swApp.Stop();
+            Console.WriteLine(swApp.ElapsedMilliseconds);
         }
     }
 }
---
+++ DAN_XLVII_Bojana_Buljic/Zadatak_1/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    enum Direction { north, south }
+
+    class Vehicle
+    {
+        //random instance for generating random directions
+        Random rnd = new Random();
+
+        #region Vehicle properties
+        //public string Name { get; set; }
+        public int OrderNo { get; set; }
+        /// <summary>
+        /// direction property of the vehicle
+        /// </summary>
+        private string direction;
+        public string DirectionV
+        {
+            get
+            {
+                return direction;
+            }
+            set
+            {
+                direction = ((Direction)rnd.Next(0, Enum.GetNames(typeof(Direction)).Length)).ToString();
+            }
+
+        }
+        #endregion
+
+        #region Constructor
+        
+        public Vehicle()
+        {
+
+
+        }
+        public Vehicle(int order)
+        {
+            OrderNo = order;
+            
+        }
+        #endregion
+    }
+}
